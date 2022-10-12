1fc7ba3859ec19257e10e8ee409caaddf4e90037
Bruce Markham <219281+brucificus@users.noreply.github.com>
2022-10-11 21:13:39 -0400

    📝 Adds Rudimentary Documentation

--- src/PanMoq.Fixturing.Primitives/ConfiguratorChainExtensions.cs
+++ src/PanMoq.Fixturing.Primitives/ConfiguratorChainExtensions.cs
@@ -4,9 +4,9 @@ public static class ConfiguratorChainExtensions
 {
     public static TSelf Configure<TSelf, TParameter1, TParameter2>(this IConfiguratorChain<TSelf, ValueTuple<TParameter1, TParameter2>> self, Action<TParameter1, TParameter2> configurator)
         where TSelf : IConfiguratorChain<TSelf, ValueTuple<TParameter1, TParameter2>> =>
-        self.Configure(t => configurator(t.Item1, t.Item2));
+        (self ?? throw new ArgumentNullException(nameof(self))).Configure(t => configurator(t.Item1, t.Item2));
 
     public static TSelf Configure<TSelf, TParameter1, TParameter2, TParameter3>(this IConfiguratorChain<TSelf, ValueTuple<TParameter1, TParameter2, TParameter3>> self, Action<TParameter1, TParameter2, TParameter3> configurator)
         where TSelf : IConfiguratorChain<TSelf, ValueTuple<TParameter1, TParameter2, TParameter3>> =>
-        self.Configure(t => configurator(t.Item1, t.Item2, t.Item3));
+        (self ?? throw new ArgumentNullException(nameof(self))).Configure(t => configurator(t.Item1, t.Item2, t.Item3));
 }
